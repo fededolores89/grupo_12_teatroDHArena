09e59b4c5c91f909a279330b7cfd4fa7d12c70e8
Carlos Olivares <focus.olivares@gmail.com>
2022-12-17 14:39:51 -0600

    agregando eventos para el nav.js

--- public/js/nav/nav.js
+++ public/js/nav/nav.js
@@ -1,1 +1,20 @@
-console.log('hi nav');
+// Elementos
+const navMobileBtn = document.querySelector('.nav-mobile-icon');
+const listMobileNav = document.querySelector('.nav-mobile-menu');
+
+// Listeners
+navMobileBtn.addEventListener('click', e => {
+  // Agregando height al menu para que sea visible
+  listMobileNav.classList.toggle('show-menu');
+
+  // Cambiando icono del boton
+  cambiarIcono(navMobileBtn);
+});
+
+// Functions
+function cambiarIcono(iconContainer) {
+  let burguerIcon = iconContainer.querySelector('.fa-bars');
+  burguerIcon.remove();
+
+  iconContainer.innerHtml = '<i>';
+}
