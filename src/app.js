a04e550f56bf5303154549795bcf4f02ccb8fba0
Martin93-css <tin.deunaa@gmail.com>
2023-01-03 23:14:53 -0300

    organisando carpetas en views y rutas en entre point

--- src/app.js
+++ src/app.js
@@ -1,40 +1,42 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-app.use(express.static('public'));
-app.listen(PORT, console.log('Servidor en http://localhost:' + PORT));
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+app.use(express.static("public"));
+app.listen(PORT, console.log("Servidor en http://localhost:" + PORT));
 
 // Declararando ubicacion del folder views
-app.set('views', path.resolve(__dirname, 'views'));
+app.set("views", path.resolve(__dirname, "views"));
 
 /*
  **Estableciendo las rutas de cada vista
  */
 
 // Home
-app.get('/', (req, res) => {
-  res.sendFile(app.get('views') + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(app.get("views") + "/main/index.html");
 });
 
 // Login
-app.get('/login', (req, res) => {
-  res.sendFile(app.get('views') + '/login.html');
+app.get("/login", (req, res) => {
+  res.sendFile(app.get("views") + "/users/login.html");
 });
 
 // Register
-app.get('/register', (req, res) => {
-  res.sendFile(app.get('views') + '/register.html');
+app.get("/register", (req, res) => {
+  res.sendFile(app.get("views") + "/users/register.html");
 });
 
 // ProductCart
-app.get('/productCart', (req, res) => {
-  res.sendFile(app.get('views') + '/productCart.html');
+app.get("/productCart", (req, res) => {
+  res.sendFile(app.get("views") + "/product/productCart.html");
 });
 
 // ProductDetail
-app.get('/productDetail', (req, res) => {
-  res.sendFile(app.get('views') + '/productDetail.html');
+app.get("/productDetail", (req, res) => {
+  res.sendFile(app.get("views") + "/product/productDetail.html");
 });
