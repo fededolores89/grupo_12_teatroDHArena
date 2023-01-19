a388a5107530e1c3e8f05301bf8a6019d2ccdf7c
Martin93-css <tin.deunaa@gmail.com>
2023-01-19 20:03:52 -0300

    agregando metodos de creacion de shows

--- src/controllers/showsController.js
+++ src/controllers/showsController.js
@@ -41,7 +41,29 @@ const controllers = {
         res.redirect("main/index");
         },
     create: (req , res) => { 
-        res.render("product/createShow")
+        // Do the magic
+    /* Incorporar FS */
+    /* Leer el archivo */
+    const shows = JSON.parse(fs.readFileSync(showsFilePath, 'utf-8'));
+    
+    
+    let productoShow = {
+    /* revisar el ultimo producto, y tomar su ID. Luego sumarle 1 */
+    id: shows[shows.length - 1].id + 1,
+    name: req.body.name,
+    price: req.body.price,
+    date: req.body.date,
+    category: req.body.category,
+    descriptionHeader: req.body.descriptionH2,
+    hour: req.body.hour,
+    image: req.file ? req.file.filename : "default-image.png"
+    }
+    /* Push */
+    products.push(productoNuevo)
+    /* Convertir a JSON */
+    /* Escribir sobre el archivo json */
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+    res.redirect("/products");
     },
 
     destroy: (req, res) => {
