5d2f17ffb3aa600fe40a1e6ce468a8ba8a80c323
Carlos Olivares <focus.olivares@gmail.com>
2023-01-28 12:06:28 -0600

    corrijiendo mensajes de error en formulario registro usuarios

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,12 +6,12 @@ const { body } = require('express-validator');
 const mainController = require("../controllers/userController")
 
 const validations = [
-  body('name').notEmpty().withMessage('Debe ingresar su nombre').isLength({ min: 3 }).withMessage('Ingrese un nombre válido'),
-  body('lastname').notEmpty().withMessage('Debe ingresar su apellido').isLength({ min: 3 }).withMessage('Ingrese un apellido válido'),
-  body('documentNum').notEmpty().withMessage('Debe ingresar su número de documento').isLength({ min: 8 }).withMessage('Ingrese un número de documento de al menos 8 carácteres'),
-  body('number').notEmpty().withMessage('Debe ingresar su teléfono').bail().isNumeric().withMessage('Ingrese solo valores numéricos en el teléfono'),
-  body('email').notEmpty().withMessage('Debe ingresar su email').bail().isEmail().withMessage('Ingrese un email válido'),
-  body('password').notEmpty().withMessage('Debe ingresar su contraseña').bail().matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).withMessage('Su contraseña debe tener al menos una letra mayúscula, una minúscula, un número y al menos 8 carácteres')
+  body('name').notEmpty().withMessage('Ingrese su nombre').bail().isLength({ min: 3 }).withMessage('Ingrese un nombre válido'),
+  body('lastname').notEmpty().withMessage('Ingrese su apellido').bail().isLength({ min: 3 }).withMessage('Ingrese un apellido válido'),
+  body('documentNum').notEmpty().withMessage('Ingrese su número de documento').bail().isLength({ min: 8 }).withMessage('Ingrese un número de documento de al menos 8 carácteres'),
+  body('number').notEmpty().withMessage('Ingrese su teléfono').bail().isNumeric().withMessage('Ingrese solo valores numéricos en el teléfono'),
+  body('email').notEmpty().withMessage('Ingrese su email').bail().isEmail().withMessage('Ingrese un email válido'),
+  body('password').notEmpty().withMessage('Ingrese su contraseña').bail().matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).withMessage('Su contraseña debe tener al menos una letra mayúscula, una minúscula, un número y al menos 8 carácteres')
 ];
 
 const storage = multer.diskStorage({
