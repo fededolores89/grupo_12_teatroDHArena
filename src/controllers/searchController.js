29fdecafb8170bd74004eac5f40933171f7fa167
facutrivisonno <facundotrivi10@gmail.com>
2023-04-20 21:57:56 -0300

    Configurando la barra de busqueda

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,11 +1,25 @@
 const db = require('../database/models');
 const sequelize = db.sequelize;
 
+const op = db.Sequelize.Op
+
 const controllers = {
     search: (req,res) =>{
-        res.send("Search")
+        
+        db.Shows.findAll({
+            where:{
+                name:  {[op.like]: "%" + req.query.search + "%"} 
+            }
+        })
+            .then(shows => {
+                res.render("product/searchShows", { shows: shows})
+                
+            })
     }
-
 }
 
 module.exports = controllers;
+
+
+
+
