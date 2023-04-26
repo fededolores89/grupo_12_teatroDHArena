416c8bc2ec25b54525ab17812ab9596a9ef78f2b
Carlos Olivares <focus.olivares@gmail.com>
2023-04-25 20:40:20 -0600

    ajustando estilos

--- src/middlewares/users/authMiddleware.js
+++ src/middlewares/users/authMiddleware.js
@@ -1,6 +1,7 @@
 const authMiddleware = (req, res, next) => {
+
   if(req.session.authUser == undefined) {
-    res.send('Debe iniciar sesion para abrir esta pagina');
+    res.redirect('/');
   } else {
     next();
   }
