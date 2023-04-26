340f3af9deadfff8500750e7b9bca98ad86e788f
Martin93-css <tin.deunaa@gmail.com>
2023-04-25 22:58:31 -0300

    ultimos detalles de las apis

--- src/database/models/Show.js
+++ src/database/models/Show.js
@@ -41,8 +41,9 @@ module.exports = (sequelize, dataTypes) => {
         detail: {
             type: dataTypes.VIRTUAL,
             get() {
+                const domain = 'http://localhost:3005/';
                 const id = this.getDataValue('id');
-                return `api/shows/${id}`;
+                return `${domain}api/shows/${id}`;
             }
         },
         imageUrl: {
